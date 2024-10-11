9d0e90820a12691f6532221e8786d11ec06624b1
Hamid Alinia <a.goodlookingboy@gmail.com>
2024-10-11 19:32:01 +0330

    add registerExtraFields

--- server/packages/server/src/global.mjs
+++ server/packages/server/src/global.mjs
@@ -200,7 +200,7 @@ let global = {
             let Settings = mongoose.model('Settings');
             let Template = mongoose.model('Template');
             let Page = mongoose.model('Page');
-            Settings.findOne({}, 'currency tax taxAmount passwordAuthentication', function (err, setting) {
+            Settings.findOne({}, 'currency tax taxAmount passwordAuthentication registerExtraFields', function (err, setting) {
                 // console.log('setting', setting)
                 Template.findOne({type: 'header'}, function (err, header) {
                     Template.findOne({type: 'footer'}, function (err, footer) {
@@ -237,6 +237,10 @@ let global = {
                             if (setting && setting.passwordAuthentication) {
                                 passwordAuthentication = setting.passwordAuthentication;
                             }
+                            let registerExtraFields = false;
+                            if (setting && setting.registerExtraFields) {
+                                registerExtraFields = setting.registerExtraFields;
+                            }
                             let taxAmount = 0;
                             if (setting && setting.taxAmount) {
                                 taxAmount = setting.taxAmount;
@@ -245,6 +249,7 @@ let global = {
                                 taxAmount: taxAmount,
                                 tax: tax,
                                 passwordAuthentication: passwordAuthentication,
+                                registerExtraFields: registerExtraFields,
                                 currency: currency,
                                 header: {
                                     maxWidth:
